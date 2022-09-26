33cd163e19dcfa19001ce6f7265f94724cf843e1
billy5982 <kuu0404@naver.com>
2022-09-26 17:10:51 +0900

    깃 배포 테스트

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,24 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { dirname } = require("path");
 module.exports = {
   entry: "./script.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
     filename: "app.bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        // use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
         exclude: /node_modules/,
+      },
+      {
+        test: /\.png$/,
+        use: ["file-loader"],
       },
     ],
   },
@@ -19,5 +26,14 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "", "index.html"),
     }),
+    new MiniCssExtractPlugin({
+      filename: "common.css",
+    }),
   ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 8080,
+  },
 };
